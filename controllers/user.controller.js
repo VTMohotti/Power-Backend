9ae0fc75a65f6c6cc76d1b69926fd172ac3a0137
VTMohotti <mohottivt@gmail.com>
2023-05-30 16:04:37 +0530

    get users and by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -73,3 +73,27 @@ exports.loginUser = async (req, res) => {
   }
 };
 
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,13 @@
-const {loginUser, registerUser} = require("../controllers/user.controller");
+const {loginUser, registerUser, getAllUsers, getUserById} = require("../controllers/user.controller");
 const router = require("express").Router();
 
 // Register a new user
 router.post("/register", registerUser);
+// Login a user
 router.post("/login", loginUser);
+// Get all users
+router.get("/", getAllUsers);
+// Get user by ID
+router.get("/:id", getUserById);
 
 module.exports = router;
